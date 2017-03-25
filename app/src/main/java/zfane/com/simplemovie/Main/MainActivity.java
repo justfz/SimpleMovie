6213dc5ae553c0aedcaaf9161aff76bdfa3d0699
ZFane <justonezf@outlook.com>
2017-03-25 14:35:42 +0800

    初始化项目 添加freeline,Retrofit 等常用库

---
+++ app/src/main/java/zfane/com/simplemovie/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package zfane.com.simplemovie.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zfane.com.simplemovie.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
